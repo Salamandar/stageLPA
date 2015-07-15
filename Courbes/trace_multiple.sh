e6c3fb1d34d20a8099912db258cc67aad0443d87
Félix Piédallu <felix@piedallu.me>
2015-07-15 15:06:36 +0200

    ajout options correctes pour le script de création de svg

--- Courbes/trace_multiple.sh
+++ Courbes/trace_multiple.sh
@@ -11,6 +11,9 @@ echo $EXECUTE
 gnuplot -persist <<- EOF
     set datafile separator ","
     set for [i=1:1000] linestyle i lw 1
+    set terminal svg size 800,600 fname 'Ubuntu' fsize 14
+    set yrange [*:0]
+    set output 'Courbes.svg'
     $EXECUTE
 EOF
 
